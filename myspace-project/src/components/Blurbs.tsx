b8c1b34f9edaed52d49adc1dfe4a9e698219a2d0
Miran Park <miranhpark@gmail.com>
2024-07-03 16:54:59 -0700

    fonts + buttons

--- myspace-project/src/components/Blurbs.tsx
+++ myspace-project/src/components/Blurbs.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Blurbs.css';
 
+import wingedHeart from '/icons/hearts/winged-heart.png';
+import bowHeart from '/icons/hearts/animated/bow-heart.gif';
+
 const Blurbs: React.FC = () => {
     return (
         <div className="blurbs">
             <h3 className="blurbs-header">Tom's Blurbs</h3>
+            <img src={wingedHeart} alt="cute winged pixel heart" />
             <h4>About me:</h4>
             <p>I'm here to help you with MySpace. Send me a message if you're confused by anything. Before asking me a question, please check the FAQ to see if your question has already been answered.</p>
             <p>I may have been on your friend list when you signed up. If you don't want me to be, click "Edit Friends" and remove me!</p>
-        </div>
+            <img src={bowHeart} alt="cute bow heartrfg
+            
+            ." />
+        </div >
     );
 };
 
--- myspace-project/src/components/Contact.tsx
+++ myspace-project/src/components/Contact.tsx
@@ -1,36 +1,28 @@
 import React from 'react';
 import './Contact.css';
 
-// import sendMessageIcon from './icons/send-message.png';
-// import forwardIcon from './icons/forward.png';
-// import addFriendIcon from './icons/add-friend.png';
-// import favoriteIcon from './icons/favorite.png';
-// import instantMessageIcon from './icons/instant-message.png';
-// import blockUserIcon from './icons/block-user.png';
-// import addToGroupIcon from './icons/add-to-group.png';
-// import rankUserIcon from './icons/rank-user.png';
+import sendMessageIcon from '/icons/bows/medium-blue.gif';
+import forwardIcon from '/icons/bows/medium-pink.gif';
+import addFriendIcon from '/icons/stars/animated/shine-blue.gif';
+import favoriteIcon from '/icons/stars/animated/shine-pink.gif';
+import instantMessageIcon from '/icons/bows/medium-mint.png';
+import blockUserIcon from '/icons/bows/medium-violet.png';
+import addToGroupIcon from '/icons/stars/animated/shine-green.gif';
+import rankUserIcon from '/icons/stars/animated/shine-purple.gif';
 
 const Contact: React.FC = () => {
     return (
         <div className="contact-box">
             <h3>Contacting Tom</h3>
             <div className="contact-buttons">
-                {/* <button><img src={sendMessageIcon} alt="" /> Send Message</button>
-            <button><img src={forwardIcon} alt="" /> Forward to Friend</button>
-            <button><img src={addFriendIcon} alt="" /> Add to Friends</button>
-            <button><img src={favoriteIcon} alt="" /> Add to Favorites</button>
-            <button><img src={instantMessageIcon} alt="" /> Instant Message</button>
-            <button><img src={blockUserIcon} alt="" /> Block User</button>
-            <button><img src={addToGroupIcon} alt="" /> Add to Group</button>
-            <button><img src={rankUserIcon} alt="" /> Rank User</button> */}
-                <button> Send Message</button>
-                <button> Forward to Friend</button>
-                <button> Add to Friends</button>
-                <button> Add to Favorites</button>
-                <button> Instant Message</button>
-                <button> Block User</button>
-                <button> Add to Group</button>
-                <button> Rank User</button>
+                <button><img src={sendMessageIcon} alt="send message icon" /> Send Message</button>
+                <button><img src={forwardIcon} alt="forward icon" /> Forward to Friend</button>
+                <button><img src={addFriendIcon} alt="add friend icon" /> Add to Friends</button>
+                <button><img src={favoriteIcon} alt="favorite icon" /> Add to Favorites</button>
+                <button><img src={instantMessageIcon} alt="instant message icon" /> Instant Message</button>
+                <button><img src={blockUserIcon} alt="block user icon" /> Block User</button>
+                <button><img src={addToGroupIcon} alt="add to group icon" /> Add to Group</button>
+                <button><img src={rankUserIcon} alt="rank user icon" /> Rank User</button>
             </div>
         </div>
     );
